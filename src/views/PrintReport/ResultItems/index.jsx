b4a10d9dd9f800ec18b3c106a9e0f0557a036f78
andouillette-ys <yangshan0823@outlook.com>
2020-07-28 22:09:27 +0800

    样式优化

--- src/views/PrintReport/ResultItems/index.jsx
+++ src/views/PrintReport/ResultItems/index.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 
+const firstRowNameIndex = [0, 1, 2, 3, 4]
+const secondRowNameIndex = [5, 6, 7, 8, 9]
+const thirdRowNameIndex = [10, 11, 12, 13, 14]
+const fifthRowNameIndex = [20, 21, 22, 23, 24]
+const itemNameRowIndex = [
+  ...firstRowNameIndex,
+  ...thirdRowNameIndex,
+  ...fifthRowNameIndex,
+]
+
 const ResultItems = ({ resultItems = [] }) => {
+  const viewItems = buildItemView(resultItems)
   return (
     <div className="item-result__items">
-      {Array.from(Array(30).keys()).map((index) => {
-        let content = ''
-        if (resultItems[index]) {
-          content = resultItems[index].itemName
-        } else if (resultItems[index - 5]) {
-          content = resultItems[index - 5].score
-        }
+      {Object.keys(viewItems).map((key, index) => {
+        const itemValueClass = itemNameRowIndex.includes(index)
+          ? ''
+          : 'item-result__items-value'
         return (
-          <div key={index} className="item-result__items-item">
-            {content}
+          <div
+            key={key}
+            className={`item-result__items-item ${itemValueClass}`}
+          >
+            {viewItems[key]}
           </div>
         )
       })}
@@ -21,3 +32,25 @@ const ResultItems = ({ resultItems = [] }) => {
 }
 
 export default ResultItems
+
+const buildItemView = (resultItems) => {
+  const result = {}
+  const rowNum = 5
+
+  resultItems.forEach((item, index) => {
+    const prop = item.scoreMode === 1 ? 'scoreLevel' : 'score'
+    if (firstRowNameIndex.includes(index)) {
+      result[index] = item.itemName
+      result[index + rowNum] = item[prop]
+    }
+    if (secondRowNameIndex.includes(index)) {
+      result[index + rowNum] = item.itemName
+      result[index + 2 * rowNum] = item[prop]
+    }
+    if (thirdRowNameIndex.includes(index)) {
+      result[index + 2 * rowNum] = item.itemName
+      result[index + 3 * rowNum] = item[prop]
+    }
+  })
+  return result
+}
