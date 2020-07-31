581d395bbe153ffa11710752b932cbbb544c6281
杨山 <shan.yang@cyber-insight.com>
2020-07-31 18:43:51 +0800

    重构新增教练

---
+++ src/views/Coach/CoachClassList/helper.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Button } from 'antd'
+import getEditClass from './EditClass'
+
+export const classListColumns = (
+  confirmDeleteClass,
+  classInEdit,
+  setClassInEdit,
+  updateClass,
+  setSelectedClass
+) => [
+  {
+    title: '序号',
+    key: 'index',
+    render: (text, record, index) => `${index + 1}`,
+  },
+  {
+    title: '班级名称',
+    dataIndex: 'name',
+    width: classInEdit ? '250px' : '250px',
+    render: (text, record) =>
+      getEditClass(record, classInEdit, setClassInEdit, updateClass),
+  },
+  {
+    title: '班级学生',
+    render: (text, record) => {
+      return (
+        <span>
+          <Button size="small" onClick={() => setSelectedClass(record)}>
+            查看班级学生
+          </Button>
+        </span>
+      )
+    },
+  },
+  {
+    title: '操作',
+    key: 'action',
+    render: (text, record) => (
+      <span
+        className="table-action"
+        onClick={() => {
+          confirmDeleteClass(record)
+        }}
+      >
+        删除
+      </span>
+    ),
+  },
+]
