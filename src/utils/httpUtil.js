5204203239921a74224a3d2b8b3351eaed3bfecc
yangshan <jjt-ys@outlook.com>
2020-08-07 22:25:51 +0800

    执行过的考试不能再更换考场

--- src/utils/httpUtil.js
+++ src/utils/httpUtil.js
@@ -1,11 +1,14 @@
-export const pathExamRounds = (examId) => {
-  return `/examination/examRoundInfo?examinationId=${examId}`
-}
+export const pathExamRounds = (examId) =>
+  `/examination/examRoundInfo?examinationId=${examId}`
 
-export const pathGroupedStudDetail = (examGroupId) => {
-  return `/examination/studentGroupedDetail?examinationGroupId=${examGroupId}`
-}
+export const pathGroupedStudDetail = (examGroupId) =>
+  `/examination/studentGroupedDetail?examinationGroupId=${examGroupId}`
 
-export const pathChangeStudGroup = (examGroupId, toRoundNum) => {
-  return `/examination/changeStudentGroup?examinationGroupId=${examGroupId}&toRoundNum=${toRoundNum}`
-}
+export const pathChangeStudGroup = (examGroupId, toRoundNum) =>
+  `/examination/changeStudentGroup?examinationGroupId=${examGroupId}&toRoundNum=${toRoundNum}`
+
+export const pathRoundAndRoom = (examId) =>
+  `/examination/getExamRoundAndItsRoom?examinationId=${examId}`
+
+export const pathUpdRoundRoom = (sourceId, newRoomId) =>
+  `/examination/updateRoundRoom?sourceId=${sourceId}&newRoomId=${newRoomId}`
--- src/views/ExamGroup/RoundAndRoom/index.jsx
+++ src/views/ExamGroup/RoundAndRoom/index.jsx
@@ -7,19 +7,16 @@ import RoundExamineeModal from './RoundExamineeModal'
 import useFetch from 'src/hooks/useFetch'
 import PageListCustom from 'src/components/PageListCustom'
 import { addRoundNumPrefix, tableOrder, getCustomRow } from 'src/utils/common'
+import { pathRoundAndRoom, pathUpdRoundRoom } from 'src/utils/httpUtil'
 
 const RoundAndRoom = ({ match }) => {
   const [selectedRound, setSelectedRound] = useState()
   const { allRooms } = useSelector((state) => state.app)
   const examId = match.params.id
-  const [roundAndRoom] = useFetch(
-    `/examination/getExamRoundAndItsRoom?examinationId=${examId}`
-  )
+  const [roundAndRoom] = useFetch(pathRoundAndRoom(examId))
 
   const updateRoundRoom = async (sourceId, newRoomId) => {
-    await api.post(
-      `/examination/updateRoundRoom?sourceId=${sourceId}&newRoomId=${newRoomId}`
-    )
+    await api.post(pathUpdRoundRoom(sourceId, newRoomId))
     message.success(`更新考场成功`)
   }
 
@@ -67,22 +64,25 @@ const getColumns = (allRooms, updateRoundRoom, setSelectedRound) => [
   {
     title: '考场',
     key: 'roomId',
-    render: (text, record) => (
-      <span>
-        <Select
-          placeholder="请选择考场"
-          style={{ width: '160px' }}
-          defaultValue={record.roomId}
-          onChange={(value) => updateRoundRoom(record.id, value)}
-        >
-          {allRooms.map((room) => (
-            <Select.Option key={room.id} value={room.id}>
-              {room.name}
-            </Select.Option>
-          ))}
-        </Select>
-      </span>
-    ),
+    render: (text, record) => {
+      return (
+        <span>
+          <Select
+            disabled={record.excuteId > 0}
+            placeholder="请选择考场"
+            style={{ width: '160px' }}
+            defaultValue={record.roomId}
+            onChange={(value) => updateRoundRoom(record.id, value)}
+          >
+            {allRooms.map((room) => (
+              <Select.Option key={room.id} value={room.id}>
+                {room.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </span>
+      )
+    },
   },
   {
     title: '操作',
