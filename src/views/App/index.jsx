59058a834cb851cedfee67adcb962d19b2064ebc
yangshan <jjt-ys@outlook.com>
2020-08-13 22:15:47 +0800

    chrome check

--- src/views/App/index.jsx
+++ src/views/App/index.jsx
@@ -14,6 +14,7 @@ import useLogin from 'src/hooks/useLogin'
 import useSearch from 'src/hooks/useSearch'
 import Router from '../Router'
 import './index.less'
+import ChromeCheck from './ChromeCheck'
 
 const App = () => {
   const dispatch = useDispatch()
@@ -65,6 +66,7 @@ const App = () => {
         </ErrorBoundary>
       </main>
       {loading && <Spin />}
+      <ChromeCheck />
     </div>
   )
 }
