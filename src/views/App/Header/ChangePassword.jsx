57f5d14918ed7b0c29b7760b9a8f606a2603adf1
杨山 <shan.yang@cyber-insight.com>
2020-08-21 18:31:26 +0800

    修复密码bug

--- src/views/App/Header/ChangePassword.jsx
+++ src/views/App/Header/ChangePassword.jsx
@@ -1,47 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Modal, Form, Icon, Input, message } from 'antd'
 import './ChangePassword.less'
 import api from 'src/utils/api'
 import { formLayout } from 'src/utils/const'
+import { pathChangePsd } from 'src/utils/httpUtil'
 
 const ChangePassword = ({ setVisible, user }) => {
-  const [confirmDirty, setConfirmDirty] = useState(false)
   const [form] = Form.useForm()
-  const { getFieldDecorator } = form
 
-  const compareToFirstPassword = (rule, value, callback) => {
-    if (value && value !== form.getFieldValue('password')) {
-      callback('两次输入的新密码不一致。')
-    } else {
-      callback()
-    }
-  }
-
-  const validateToNextPassword = (rule, value, callback) => {
-    if (value && confirmDirty) {
-      form.validateFields(['confirm'], { force: true })
-    }
-    callback()
-  }
-
-  const handleConfirmBlur = (e) => {
-    const { value } = e.target
-    setConfirmDirty(confirmDirty || !!value)
-  }
-
-  const handleOk = () => {
-    form.validateFields(async (err, values) => {
-      if (!err) {
-        const { oldPassword, password } = values
-        await api.post(
-          `/user/changePsw?&oldPassword=${oldPassword}&newPsw=${password}`
-        )
-        message.success('密码修改成功。')
-        setVisible(false)
-      } else {
-        message.error('请输入正确的密码。')
-      }
-    })
+  const handleOk = async () => {
+    const { oldPassword, password: newPsw } = await form.validateFields([
+      'oldPassword',
+      'password',
+      'confirm',
+    ])
+    await api.post(pathChangePsd({ oldPassword, newPsw }))
+    message.success('密码修改成功。')
+    setVisible(false)
   }
 
   return (
@@ -58,49 +33,46 @@ const ChangePassword = ({ setVisible, user }) => {
           <Form.Item label="用户名称">
             <span>{user?.username}</span>
           </Form.Item>
-          <Form.Item label="旧密码">
-            {getFieldDecorator('oldPassword', {
-              rules: [{ required: true }],
-            })(
-              <Input
-                prefix={<Icon type="lock" />}
-                type="password"
-                placeholder="请输入旧密码"
-              />
-            )}
+          <Form.Item
+            name="oldPassword"
+            label="旧密码"
+            rules={[{ required: true }]}
+            hasFeedback
+          >
+            <Input.Password
+              prefix={<Icon type="lock" />}
+              placeholder="请输入旧密码"
+            />
           </Form.Item>
-          <Form.Item label="新密码">
-            {getFieldDecorator('password', {
-              rules: [
-                { required: true },
-                {
-                  validator: validateToNextPassword,
-                },
-              ],
-            })(
-              <Input
-                prefix={<Icon type="lock" />}
-                type="password"
-                placeholder="请输入新密码"
-              />
-            )}
+          <Form.Item
+            name="password"
+            label="新密码"
+            rules={[{ required: true, min: 6 }]}
+            hasFeedback
+          >
+            <Input.Password
+              prefix={<Icon type="lock" />}
+              placeholder="请输入新密码"
+            />
           </Form.Item>
-          <Form.Item label="确认密码">
-            {getFieldDecorator('confirm', {
-              rules: [
-                { required: true },
-                {
-                  validator: compareToFirstPassword,
+          <Form.Item
+            name="confirm"
+            label="确认密码"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              { required: true },
+              ({ getFieldValue }) => ({
+                validator(rule, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject('两次输入的密码不一致')
                 },
-              ],
-            })(
-              <Input
-                prefix={<Icon type="lock" />}
-                type="password"
-                placeholder="请再次输入新密码"
-                onBlur={handleConfirmBlur}
-              />
-            )}
+              }),
+            ]}
+          >
+            <Input.Password />
           </Form.Item>
         </Form>
       </Modal>
