1aadf124c24117440ad351f8badefaae825f1543
yangshan <jjt-ys@outlook.com>
2020-10-20 22:50:52 +0800

    分组重构

--- src/views/ExamGroup/RoundAndRoom/index.jsx
+++ src/views/ExamGroup/RoundAndRoom/index.jsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react'
 import './index.less'
-import api from 'src/utils/api'
-import { message, Select, Tag } from 'antd'
+
+import { message } from 'antd'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import RoundExamineeModal from './RoundExamineeModal'
 import PageListCustom from 'src/components/PageListCustom'
-import { addRoundNumPrefix, tableOrder, getCustomRow } from 'src/utils/common'
+import useFetch from 'src/hooks/useFetch'
+import api from 'src/utils/api'
 import {
+  pathExam,
   pathRoundAndRoom,
   pathUpdRoundRoom,
-  pathExam,
 } from 'src/utils/httpUtil'
-import CustomTable from 'src/components/CustomTable'
-import useTableFetch from 'src/hooks/useTableFetch'
-import useFetch from 'src/hooks/useFetch'
+
+import RoundExamineeModal from './RoundExamineeModal'
+import Rounds from './Rounds'
 
 const RoundAndRoom = ({ match }) => {
+  const examId = match.params.id
   const [selectedRound, setSelectedRound] = useState()
   const { allRooms } = useSelector((state) => state.app)
-  const examId = match.params.id
   const [exam = {}] = useFetch(pathExam(examId))
-  const tableList = useTableFetch(pathRoundAndRoom, { examinationId: examId })
+  const [{ data } = {}] = useFetch(
+    `${pathRoundAndRoom}?page=1&rows=10000&examinationId=${examId}`
+  )
 
   const updateRoundRoom = async (sourceId, newRoomId) => {
     await api.post(pathUpdRoundRoom(sourceId, newRoomId))
@@ -31,17 +33,11 @@ const RoundAndRoom = ({ match }) => {
     setSelectedRound()
   }
 
+  if (!data) return null
+
   return (
-    <PageListCustom
-      title={`${exam.title}考场分配`}
-      customClass="round-room-list"
-    >
-      <CustomTable
-        {...tableList}
-        className="round-room-table"
-        columns={getColumns(allRooms, updateRoundRoom, setSelectedRound)}
-        rowKey="round_num"
-      />
+    <div>
+      <Rounds allRounds={data} />
       {selectedRound && (
         <RoundExamineeModal
           examinationId={examId}
@@ -49,55 +45,8 @@ const RoundAndRoom = ({ match }) => {
           hideModal={hideRoundExamineeModal}
         />
       )}
-    </PageListCustom>
+    </div>
   )
 }
 
 export default RoundAndRoom
-
-const getColumns = (allRooms, updateRoundRoom, setSelectedRound) => [
-  { ...tableOrder, width: 80 },
-  getCustomRow('组号', (record) => addRoundNumPrefix(record.round_num), 80),
-  {
-    title: '教练',
-    width: 300,
-    render: (text, record) => (
-      <>
-        {record.coachs.map(({ nickname, id }) => (
-          <Tag key={id}>{nickname}</Tag>
-        ))}
-      </>
-    ),
-  },
-  {
-    title: '考场',
-    key: 'roomId',
-    render: (text, record) => {
-      return (
-        <span>
-          <Select
-            disabled={record.excuteId > 0}
-            placeholder="请选择考场"
-            style={{ width: '160px' }}
-            defaultValue={record.roomId}
-            onChange={(value) => updateRoundRoom(record.id, value)}
-          >
-            {allRooms.map((room) => (
-              <Select.Option key={room.id} value={room.id}>
-                {room.name}
-              </Select.Option>
-            ))}
-          </Select>
-        </span>
-      )
-    },
-  },
-  {
-    title: '操作',
-    render: (text, record) => (
-      <span className="table-action" onClick={() => setSelectedRound(record)}>
-        查看分组考生
-      </span>
-    ),
-  },
-]
