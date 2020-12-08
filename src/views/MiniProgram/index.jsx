69c9de205630b4fceb08e949be78907ebe290832
杨山 <shan.yang@cyber-insight.com>
2020-12-08 10:08:36 +0800

    成绩单

--- src/views/MiniProgram/index.jsx
+++ src/views/MiniProgram/index.jsx
@@ -4,11 +4,14 @@ import React from 'react'
 import useSearch from 'src/hooks/useSearch'
 import QrCode from './QrCode'
 import ExamResult from './ExamResult'
+import useWindowSize from 'src/hooks/useWindowSize'
 
 const MiniProgram = () => {
   const { page } = useSearch()
+  const { width: windowWidth } = useWindowSize()
+
   return (
-    <div className={`mini-program ${page}`}>
+    <div className={`mini-program ${page} ${getClass(windowWidth)}`}>
       {page === 'qrcode' && <QrCode />}
       {page === 'examResult' && <ExamResult />}
     </div>
@@ -16,3 +19,14 @@ const MiniProgram = () => {
 }
 
 export default MiniProgram
+
+const getClass = (windowWidth) => {
+  let width = 320
+  if (windowWidth > 320 && windowWidth <= 375) {
+    width = 375
+  }
+  if (windowWidth > 375 && windowWidth <= 414) {
+    width = 414
+  }
+  return `width-${width}`
+}
